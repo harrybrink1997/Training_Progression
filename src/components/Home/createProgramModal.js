fd8e831e6566c38a7b28fc80e5db733ba871ee5e
unknown <harry.brink@skyciv.com>
2020-12-07 16:17:08 +1100

    Creaste new program looking fresh work with varousel type modal

--- src/components/Home/createProgramModal.js
+++ src/components/Home/createProgramModal.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import './home.css'
-import { Modal, Button, Form, Input, Container, Popup, Icon, Label, Grid } from 'semantic-ui-react'
+import { Modal, Button, Form, Input, Container, Popup, Icon, Label, Grid, Breadcrumb } from 'semantic-ui-react'
 
 import SemanticDatepicker from 'react-semantic-ui-datepickers';
 
@@ -19,6 +19,11 @@ const CreateProgramModal = ({ handleFormSubmit }) => {
     const [date, setDate] = useState(new Date())
     const [pageNum, setPageNum] = useState(1)
     const [goalList, setGoalList] = useState(() => { return {} })
+
+    const handlePageChange = (event, pageNum) => {
+        event.preventDefault()
+        setPageNum(pageNum)
+    }
 
     const generateTodaysDate = (inputDay) => {
 
@@ -99,6 +104,12 @@ const CreateProgramModal = ({ handleFormSubmit }) => {
         setGoalList(newGoalList)
     }
 
+    const handleNonFinalSubmit = (event) => {
+        event.preventDefault()
+        setPageNum(prevNum => prevNum + 1)
+
+    }
+
     return (
         <Modal
             size='small'
@@ -109,159 +120,224 @@ const CreateProgramModal = ({ handleFormSubmit }) => {
             trigger={<Button className='lightPurpleButton'>Create Program</Button>}
         >
             <Modal.Header>Create A Program</Modal.Header>
-            <Form onSubmit={handleSubmit}>
-                <Modal.Content>
-                    <Container>
-                        <Form.Field>
-                            <InputLabel text='Program Name' />
-                            <Input
-                                fluid
-                                value={programName}
-                                onChange={changeProgramName}
-                                required
-                            />
-                        </Form.Field>
-                        <Form.Group widths='equal'>
+            <Modal.Content>
+                <Breadcrumb>
+                    {
+                        (pageNum >= 1) ? (pageNum == 1)
+                            ?
+                            <Breadcrumb.Section link active >Program Name</Breadcrumb.Section>
+                            :
+                            <Breadcrumb.Section link onClick={(e) => handlePageChange(e, 1)}>Program Name</Breadcrumb.Section>
+                            :
+                            <></>
+
+                    }
+                    {
+                        pageNum >= 2 &&
+                        <Breadcrumb.Divider>/</Breadcrumb.Divider>
+                    }
+                    {
+                        (pageNum >= 2) ? (pageNum == 2)
+                            ?
+                            <Breadcrumb.Section link active>Loading Timeframes</Breadcrumb.Section>
+                            :
+                            <Breadcrumb.Section link onClick={(e) => handlePageChange(e, 2)}>Loading Timeframes</Breadcrumb.Section>
+                            :
+                            <></>
+                    }
+                    {
+                        pageNum >= 3 &&
+                        <Breadcrumb.Divider>/</Breadcrumb.Divider>
+                    }
+                    {
+                        (pageNum >= 3) ? (pageNum == 3)
+                            ?
+                            <Breadcrumb.Section link active>Loading Scheme</Breadcrumb.Section>
+                            :
+                            <Breadcrumb.Section link onClick={(e) => handlePageChange(e, 3)}>Loading Scheme</Breadcrumb.Section>
+                            :
+                            <></>
+                    }
+                    {
+                        pageNum >= 4 &&
+                        <Breadcrumb.Divider>/</Breadcrumb.Divider>
+                    }
+                    {
+                        (pageNum >= 4) ? (pageNum == 4)
+                            ?
+                            <Breadcrumb.Section link active>Goals</Breadcrumb.Section>
+                            :
+                            <Breadcrumb.Section link onClick={(e) => handlePageChange(e, 3)}>Goals</Breadcrumb.Section>
+                            :
+                            <></>
+                    }
+                    {
+                        pageNum == 5 &&
+                        <Breadcrumb.Divider>/</Breadcrumb.Divider>
+                    }
+                    {
+                        pageNum == 5 &&
+                        <Breadcrumb.Section link active>Start Date</Breadcrumb.Section>
+                    }
+                </Breadcrumb>
+                <Container>
+                    {
+                        pageNum == 1 &&
+                        <Form onSubmit={handleNonFinalSubmit}>
                             <Form.Field>
-                                <InputLabel
-                                    text='Acute Workload Timeframe (Measured in Days) &nbsp;'
-                                    toolTip={<Popup
-                                        trigger={<Icon name='question circle outline' />}
-                                        content='(Recommended 7)'
-                                        position='right center'
-                                    />}
-                                />
                                 <Input
                                     fluid
-                                    value={acutePeriod}
-                                    onChange={changeAcutePeriod}
+                                    autoFocus={true}
+                                    value={programName}
+                                    onChange={changeProgramName}
                                     required
                                 />
                             </Form.Field>
+                            {
+                                (programName != '') ?
+                                    <Button className='submitBtn' type="submit">Next</Button>
+                                    :
+                                    <></>
 
+                            }
+                        </Form>
+
+                    }
+                    {
+                        pageNum == 2 &&
+                        <Form onSubmit={handleNonFinalSubmit} >
+                            <Form.Group widths='equal'>
+                                <Form.Field>
+                                    <InputLabel
+                                        text='Acute Workload Timeframe (Measured in Days) &nbsp;'
+                                        toolTip={<Popup
+                                            trigger={<Icon name='question circle outline' />}
+                                            content='(Recommended 7)'
+                                            position='right center'
+                                        />}
+                                    />
+                                    <Input
+                                        fluid
+                                        autoFocus={true}
+                                        value={acutePeriod}
+                                        onChange={changeAcutePeriod}
+                                        required
+                                    />
+                                </Form.Field>
+
+                                <Form.Field>
+                                    <InputLabel
+                                        text='Chronic Workload Timeframe (Measured in Days) &nbsp;'
+                                        toolTip={<Popup
+                                            trigger={<Icon name='question circle outline' />}
+                                            content='(Recommended 28)'
+                                            position='right center'
+                                        />}
+                                    />
+                                    <Input
+                                        fluid
+                                        value={chronicPeriod}
+                                        onChange={changeChronicPeriod}
+                                        required
+                                    />
+                                </Form.Field>
+                            </Form.Group>
+                            <Button className='submitBtn' type="submit">Next</Button>
+                        </Form>
+                    }
+                    {
+                        pageNum == 3 &&
+                        <Form onSubmit={handleNonFinalSubmit}>
                             <Form.Field>
                                 <InputLabel
-                                    text='Chronic Workload Timeframe (Measured in Days) &nbsp;'
+                                    text='Selected A Loading Scheme &nbsp;'
                                     toolTip={<Popup
                                         trigger={<Icon name='question circle outline' />}
-                                        content='(Recommended 28)'
+                                        content='This is the loading scheme tooltip'
                                         position='right center'
                                     />}
                                 />
-                                <Input
-                                    fluid
-                                    value={chronicPeriod}
-                                    onChange={changeChronicPeriod}
-                                    required
+                                <Form.Radio
+                                    label='RPE / Time'
+                                    value='rpe_time'
+                                    checked={loadingScheme === 'rpe_time'}
+                                    onChange={handleLoadingSchemeChange}
+                                />
+                                <Form.Radio
+                                    label='Weight / Repetitions'
+                                    value='weight_reps'
+                                    checked={loadingScheme === 'weight_reps'}
+                                    onChange={handleLoadingSchemeChange}
                                 />
                             </Form.Field>
-                        </Form.Group>
+                            <Button className='submitBtn' type="submit">Next</Button>
+                        </Form>
+                    }
+                    {
+                        pageNum == 4 &&
+                        <Form onSubmit={handleNonFinalSubmit}>
+                            <Form.Field>
+                                <div id='hpModalGoalsLabelContainer'>
+                                    <InputLabel
+                                        text='Program Goals &nbsp;'
+                                    />
+                                    <Icon
+                                        className='hpModalModifyNumGoalsBtn'
+                                        style={{ color: 'white' }}
+                                        name='minus square outline'
+                                        onClick={() => handleGoalNumUpdate(false)}
+                                    />
+                                    <Icon
+                                        className='cpModalModifyNumGoalsBtn'
+                                        style={{ color: 'white' }}
+                                        name='plus square outline'
+                                        onClick={() => handleGoalNumUpdate(true)}
+                                    />
+                                </div>
+                                <div id='hpModalGoalsInputContainer'>
+                                    {
+                                        Object.values(goalList).map((value, index) => {
+                                            return (
+                                                <div key={index}>
+                                                    {value.formHTML()}
+                                                </div>
+                                            )
+                                        })
+                                    }
+                                </div>
+                            </Form.Field>
+                            <Button className='submitBtn' type="submit">Next</Button>
 
-                        <Grid columns='equal'>
-                            <Grid.Column>
-                                <Form.Field>
-                                    <InputLabel
-                                        text='Loading Scheme &nbsp;'
-                                        toolTip={<Popup
-                                            trigger={<Icon name='question circle outline' />}
-                                            content='This is the loading scheme tooltip'
-                                            position='right center'
-                                        />}
-                                    />
-                                    <Form.Radio
-                                        label='RPE / Time'
-                                        value='rpe_time'
-                                        checked={loadingScheme === 'rpe_time'}
-                                        onChange={handleLoadingSchemeChange}
-                                    />
-                                    <Form.Radio
-                                        label='Weight / Repetitions'
-                                        value='weight_reps'
-                                        checked={loadingScheme === 'weight_reps'}
-                                        onChange={handleLoadingSchemeChange}
-                                    />
-                                </Form.Field>
-                            </Grid.Column>
-                            <Grid.Column>
-                                <Form.Field>
-                                    <InputLabel
-                                        text='Starting Date &nbsp;'
-                                        toolTip={<Popup
-                                            trigger={<Icon name='question circle outline' />}
-                                            content='This is the date tooltip'
-                                            position='right center'
-                                        />}
-                                    />
-                                    <SemanticDatepicker
-                                        today
-                                        type='basic'
-                                        onChange={handleDateChange}
-                                        format='DD-MM-YYYY'
-                                        value={date}
-                                    />
-                                </Form.Field>
-                            </Grid.Column>
-                        </Grid>
-                        <Form.Field>
-                            <div id='hpModalGoalsLabelContainer'>
+                        </Form>
+                    }
+                    {
+                        pageNum == 5 &&
+                        <Form onSubmit={handleSubmit}>
+                            <Form.Field>
                                 <InputLabel
-                                    text='Program Goals &nbsp;'
+                                    text='Select Starting Date &nbsp;'
+                                    toolTip={<Popup
+                                        trigger={<Icon name='question circle outline' />}
+                                        content='This is the date tooltip'
+                                        position='right center'
+                                    />}
                                 />
-                                <Icon
-                                    className='hpModalModifyNumGoalsBtn'
-                                    style={{ color: 'white' }}
-                                    name='minus square outline'
-                                    onClick={() => handleGoalNumUpdate(false)}
+                                <SemanticDatepicker
+                                    today
+                                    type='basic'
+                                    onChange={handleDateChange}
+                                    format='DD-MM-YYYY'
+                                    value={date}
                                 />
-                                <Icon
-                                    className='cpModalModifyNumGoalsBtn'
-                                    style={{ color: 'white' }}
-                                    name='plus square outline'
-                                    onClick={() => handleGoalNumUpdate(true)}
-                                />
-                            </div>
-                            <div id='hpModalGoalsInputContainer'>
-                                {
-                                    Object.values(goalList).map((value, index) => {
-                                        return (
-                                            <div key={index}>
-                                                {value.formHTML()}
-                                            </div>
-                                        )
-                                    })
-                                }
-                            </div>
+                            </Form.Field>
+                            <Button className='submitBtn' type="submit">Create Program</Button>
+                        </Form>
+                    }
 
-                        </Form.Field>
-                    </Container>
-                </Modal.Content>
-                <Modal.Actions>
-                    <Button onClick={() => setShow(false)}>Close</Button>
-                    <Button className='submitBtn' type="submit">Create Program</Button>
-                </Modal.Actions>
-            </Form>
+                </Container>
+            </Modal.Content>
         </Modal >
     );
 }
-
-// const initialGoalListState = []
-
-// const reducer = (state, action) => {
-//     switch (action.type) {
-//         case 'add':
-//             var newGoalIndex = Object.keys(state).length
-//             let newState = { ...state }
-//             newState[newGoalIndex] = action.item
-//             return newState
-//             newGoalList[newGoalIndex] = new Goal(newGoalIndex, updateGoalList)
-//         case 'remove':
-
-
-//         case 'update':
-
-//         default:
-//             throw new Error()
-//     }
-// }
 
 export default CreateProgramModal;
