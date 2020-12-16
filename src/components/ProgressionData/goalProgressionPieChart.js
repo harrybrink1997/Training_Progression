c5c64a8ec2cdbf17a15fc0a0bd8324e670c3d76f
unknown <harry.brink@skyciv.com>
2020-12-16 15:55:43 +1100

    fixed minor bug i didn't see in pie chart labelling

--- src/components/ProgressionData/goalProgressionPieChart.js
+++ src/components/ProgressionData/goalProgressionPieChart.js
@@ -21,18 +21,6 @@ const renderActiveShape = (props) => {
 
     return (
         <g>
-            {/* <text scaleToFit={true} x={cx} y={cy} dy={8} textAnchor="middle" fill='white' className="recharts-text recharts-label">
-                <tspan>
-                    {payload.name}
-                </tspan>
-                <tspan>
-                    {`${value} Completed`}
-                </tspan>
-            </text> */}
-            {/* <Label width={30} position='center'
-            content={}
-            >
-            </Label> */}
             <Sector
                 cx={cx}
                 cy={cy}
@@ -51,19 +39,9 @@ const renderActiveShape = (props) => {
                 outerRadius={outerRadius + 10}
                 fill={fill}
             />
-            {/* <path d={`M${sx},${sy}L${mx},${my}L${ex},${ey}`} stroke={fill} fill="none" />
-            <circle cx={ex} cy={ey} r={2} fill={fill} stroke="none" />
-                <text x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} textAnchor={textAnchor} fill="white">{`${value} Completed`}</text>
-                <text x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} dy={18} textAnchor={textAnchor} fill="#999">
-                    {`${(percent * 100).toFixed(2)}%`}
-                </text> */}
         </g>
     );
 };
-
-const CustomPieChartLabel = () => {
-
-}
 
 const GoalProgressionPieChart = ({ data, chartColours }) => {
 
