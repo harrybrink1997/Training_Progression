f9f68e542f77fc3773fcef4dd7bfd1343b95fa64
unknown <harry.brink@skyciv.com>
2021-01-10 12:49:42 +1100

    teams creation and management coming along lol not much else to say

--- src/components/CustomComponents/programAssignment.js
+++ src/components/CustomComponents/programAssignment.js
@@ -5,20 +5,55 @@ import InputLabel from './DarkModeInput'
 
 import RowSelectTable from './rowSelectTable'
 import BasicTable from './basicTable'
+import { im } from 'mathjs'
 
 const ProgramAssignment = ({ handleFormSubmit, programTableData, programTableColumns }) => {
 
     const [pageNum, setPageNum] = useState(1)
     const [access, setAccess] = useState('')
 
+    const initTableData = (rawData) => {
+        console.log(rawData)
+        var tableData = []
+        Object.values(rawData).forEach(program => {
+            tableData.push({
+                program: program.original.program,
+                acutePeriod: program.original.acutePeriod,
+                chronicPeriod: program.original.chronicPeriod,
+                programLength: program.original.programLength,
+            })
+        })
+        return tableData
+    }
+
+    const programTableDataFormatted = initTableData(programTableData)
+
     const handlePageChange = (event, pageNum) => {
         event.preventDefault()
         setPageNum(pageNum)
     }
-    const handleSubmit = (accessType) => {
+    const handleSubmit = (accessType, programData = undefined) => {
+        var payLoad = {}
         if (accessType === 'unlimited') {
-            console.log('is unlimited')
+            payLoad.sequential = false
+            payLoad.unlimited = []
+
+            programTableData.forEach(program => {
+                payLoad.unlimited.push(program.original)
+            })
+
+            console.log(payLoad)
+        } else if (accessType === 'sequential') {
+            payLoad.unlimited = false
+            payLoad.sequential = programData
+
+            console.log(payLoad)
+        } else if (accessType === 'mixed') {
+            payLoad = programData
+
+            console.log(payLoad)
         }
+
         setAccess('')
     }
 
@@ -35,6 +70,15 @@ const ProgramAssignment = ({ handleFormSubmit, programTableData, programTableCol
     const resetPages = () => {
         setAccess('')
     }
+
+    const handleSequentialProgramSubmission = (programOrder) => {
+        handleSubmit('sequential', programOrder)
+    }
+
+    const handleMixProgramSubmission = (programData) => {
+        handleSubmit('mixed', programData)
+    }
+
 
     useEffect(() => {
         console.log(access)
@@ -69,12 +113,12 @@ const ProgramAssignment = ({ handleFormSubmit, programTableData, programTableCol
                             </Card>
                         </div>
                         <div>
-                            <Card onClick={() => { processAccess('mix') }}>
+                            <Card onClick={() => { processAccess('mixed') }}>
                                 <Card.Content className='iconContent'>
                                     <Icon name='cogs' size='huge' />
                                 </Card.Content>
                                 <Card.Content>
-                                    <Card.Header >Mix <br /> Access</Card.Header>
+                                    <Card.Header >Mixed <br /> Access</Card.Header>
                                     <Card.Description>Athletes will have unlimited access to selected programs and sequential access to selected programs.</Card.Description>
                                 </Card.Content>
                             </Card>
@@ -85,8 +129,17 @@ const ProgramAssignment = ({ handleFormSubmit, programTableData, programTableCol
             {
                 access === 'sequential' &&
                 <SequentialAccess
-                    programTableData={programTableData}
+                    programTableData={programTableDataFormatted}
                     programTableColumns={programTableColumns}
+                    handleFormSubmit={handleSequentialProgramSubmission}
+                />
+            }
+            {
+                access === 'mixed' &&
+                <MixedAccess
+                    programTableData={programTableDataFormatted}
+                    programTableColumns={programTableColumns}
+                    handleFormSubmit={handleMixProgramSubmission}
                 />
             }
             {
@@ -107,27 +160,10 @@ const ProgramAssignment = ({ handleFormSubmit, programTableData, programTableCol
 
 const SequentialAccess = ({ handleFormSubmit, programTableData, programTableColumns }) => {
 
-    const [pageNum, setPageNum] = useState(1)
-    const [access, setAccess] = useState('')
     const [orderedPrograms, setOrderedPrograms] = useState([])
     const tableColumns = programTableColumns
 
-    const initSeqTableData = (rawData) => {
-        console.log(rawData)
-        var tableData = []
-        Object.values(rawData).forEach(program => {
-            tableData.push({
-                program: program.original.program,
-                acutePeriod: program.original.acutePeriod,
-                chronicPeriod: program.original.chronicPeriod,
-                programLength: program.original.programLength,
-            })
-        })
-        return tableData
-    }
-
-    // const [data, setData] = useState(initSeqTableData(programTableData))
-    const [seqTableData, setSeqTableData] = useState(initSeqTableData(programTableData))
+    const [seqTableData, setSeqTableData] = useState(programTableData)
 
     const programAlreadyOrdered = (programName) => {
         if (orderedPrograms.length === 0) {
@@ -201,22 +237,20 @@ const SequentialAccess = ({ handleFormSubmit, programTableData, programTableColu
         console.log(orderedPrograms)
     }, [orderedPrograms, seqTableData])
 
-    const handlePageChange = (event, pageNum) => {
-        event.preventDefault()
-        setPageNum(pageNum)
-    }
     const handleSubmit = (event) => {
-        setAccess('')
+        handleFormSubmit(orderedPrograms)
+        setOrderedPrograms([])
     }
 
-    const handleNonFinalSubmit = (event) => {
-        event.preventDefault()
-        setPageNum(prevNum => prevNum + 1)
+    const removeLatestProgram = () => {
+        let newOrderedData = [...orderedPrograms]
+        var programToRemove = newOrderedData.pop()
+        delete programToRemove.order
+        let newSeqTableData = [...seqTableData]
+        newSeqTableData.push(programToRemove)
 
-    }
-
-    const resetPages = () => {
-        setPageNum(1)
+        setOrderedPrograms(newOrderedData)
+        setSeqTableData(newSeqTableData)
     }
 
     return (
@@ -229,9 +263,6 @@ const SequentialAccess = ({ handleFormSubmit, programTableData, programTableColu
                     rowSelectChangeHanlder={handleProgramSelection}
                 />
             }
-            <br />
-            <br />
-            <br />
             {
                 orderedPrograms.length > 0 &&
                 <div>
@@ -240,23 +271,212 @@ const SequentialAccess = ({ handleFormSubmit, programTableData, programTableColu
                         columns={orderedProgramsTableColumns}
                     />
                     <div className='rightAlignedButton'>
+                        {
+                            seqTableData.length === 0 &&
+                            <Button
+                                icon
+                                className='tick-inverted'
+                                onClick={() => handleSubmit()}
+                            >
+                                <Icon name='check' />
+                            </Button>
+                        }
                         <Button
                             icon
-                            className='red-undo-inverted'
-                        // onClick={}
+                            className='undo-inverted'
+                            onClick={() => removeLatestProgram()}
                         >
                             <Icon name='undo' />
                         </Button>
                     </div>
                 </div>
-
             }
 
         </div>
-
     );
 }
 
 
+const UnlimitedAccess = ({ handleFormSubmit, programTableData, programTableColumns }) => {
+
+    const [unlimitedPrograms, setUnlimitedPrograms] = useState([])
+    const tableColumns = programTableColumns
+
+    const [unlimTableData, setUnlimTableData] = useState(programTableData)
+
+    const UnlimitedProgramsTableColumns = [
+        {
+            Header: 'Program',
+            accessor: 'program',
+        },
+        {
+            Header: 'Acute Period',
+            accessor: 'acutePeriod',
+        },
+        {
+            Header: 'Chronic Period',
+            accessor: 'chronicPeriod',
+        },
+        {
+            Header: 'Program Length (Weeks)',
+            accessor: 'programLength',
+        },
+    ]
+
+
+    const handleSubmit = (event) => {
+        console.log(unlimTableData)
+        console.log(unlimitedPrograms)
+
+        handleFormSubmit(unlimitedPrograms, unlimTableData)
+
+    }
+
+    const removeLatestProgram = () => {
+        let newUnlimData = [...unlimitedPrograms]
+        var programToRemove = newUnlimData.pop()
+        delete programToRemove.order
+        let newUnlimTableData = [...unlimTableData]
+        newUnlimTableData.push(programToRemove)
+
+        setUnlimitedPrograms(newUnlimData)
+        setUnlimTableData(newUnlimTableData)
+    }
+
+    const programAlreadyOrdered = (programName) => {
+        if (unlimitedPrograms.length === 0) {
+            return false
+        } else {
+            for (var program in unlimitedPrograms) {
+                if (unlimitedPrograms[program].program === programName) {
+                    return true
+                }
+            }
+            return false
+        }
+    }
+
+    const removeProgramFromSequentialTable = (programName) => {
+
+        for (var program in unlimTableData) {
+            if (unlimTableData[program].program === programName) {
+                let newTableData = [...unlimTableData]
+                newTableData.splice(program, 1)
+                setUnlimTableData(newTableData)
+                return
+            }
+        }
+    }
+
+    const handleProgramSelection = (selectionData) => {
+        for (var row in selectionData) {
+            var programData = selectionData[row].original
+            if (programAlreadyOrdered(programData.program)) {
+                continue
+            } else {
+                let newUnlimitedPrograms = [...unlimitedPrograms]
+                newUnlimitedPrograms.push(programData)
+                setUnlimitedPrograms(newUnlimitedPrograms)
+                removeProgramFromSequentialTable(programData.program)
+            }
+
+        }
+    }
+
+    return (
+        <div>
+            {
+                unlimTableData.length > 0 &&
+                <RowSelectTable
+                    data={unlimTableData}
+                    columns={tableColumns}
+                    rowSelectChangeHanlder={handleProgramSelection}
+                />
+            }
+            {
+                unlimitedPrograms.length > 0 &&
+                <div>
+                    <BasicTable
+                        data={unlimitedPrograms}
+                        columns={UnlimitedProgramsTableColumns}
+                    />
+                    <div className='rightAlignedButton'>
+
+                        <Button
+                            icon
+                            className='tick-inverted'
+                            onClick={() => handleSubmit()}
+                        >
+                            <Icon name='check' />
+                        </Button>
+                        <Button
+                            icon
+                            className='undo-inverted'
+                            onClick={() => removeLatestProgram()}
+                        >
+                            <Icon name='undo' />
+                        </Button>
+                    </div>
+                </div>
+            }
+
+        </div>
+    );
+
+}
+
+
+const MixedAccess = ({ handleFormSubmit, programTableData, programTableColumns }) => {
+
+    const [unlimitedPrograms, setUnlimitedPrograms] = useState([])
+    const [unlimitedProgramsSelected, setUnlimitedProgramsSelected] = useState(false)
+    const [sequentialProgramsTableData, setSequentialProgramsTableData] = useState([])
+
+    const handleUnlimitedProgramsSubmitted = (unlimitedProgramsSelected, sequentialPrograms) => {
+        console.log(unlimitedProgramsSelected)
+        console.log(sequentialPrograms)
+
+        if (sequentialPrograms.length > 0) {
+            setUnlimitedProgramsSelected(true)
+            setSequentialProgramsTableData(sequentialPrograms)
+            setUnlimitedPrograms(unlimitedProgramsSelected)
+        } else {
+            handleFormSubmit({
+                unlimited: unlimitedProgramsSelected,
+                sequential: false
+            })
+        }
+    }
+
+    const handleSequentialProgramSubmission = (orderedPrograms) => {
+        console.log(orderedPrograms)
+        handleFormSubmit({
+            unlimited: unlimitedPrograms,
+            sequential: orderedPrograms
+        })
+    }
+
+    return (
+        <div>
+            {
+                !unlimitedProgramsSelected &&
+                <UnlimitedAccess
+                    programTableData={programTableData}
+                    programTableColumns={programTableColumns}
+                    handleFormSubmit={handleUnlimitedProgramsSubmitted}
+                />
+            }
+            {
+                unlimitedProgramsSelected &&
+                <SequentialAccess
+                    programTableData={sequentialProgramsTableData}
+                    programTableColumns={programTableColumns}
+                    handleFormSubmit={handleSequentialProgramSubmission}
+                />
+            }
+        </div>
+    )
+
+}
 
 export default ProgramAssignment;
